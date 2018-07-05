f30e692c9d42f5d495ae15d23b6b446d8d0f714c
Charly <charly.strn@gmail.com>
2018-07-05 11:53:48 +0200

    PUSHD

--- data.php
+++ data.php
@@ -9,7 +9,7 @@ if(isset($_POST['pseudo']) && isset($_POST['platform'])){
   curl_setopt($ch, CURLOPT_HEADER, FALSE);
   curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
   curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-      'TRN-Api-Key:'
+      'TRN-Api-Key: b8822cb4-20ff-4868-a704-46173e32c106'
   ));
   $response = curl_exec($ch);
   curl_close($ch);
@@ -19,7 +19,7 @@ if(isset($_POST['pseudo']) && isset($_POST['platform'])){
   $string = file_get_contents("stats.json");
   $json_a = json_decode($string, true);
 
-  //print_r($json_a);
+
 }
 
 ?>
@@ -50,7 +50,7 @@ if(isset($_POST['pseudo']) && isset($_POST['platform'])){
 <!--===============================================================================================-->
 </head>
 <body>
-  <h1> <?php if(isset($json_a['epicUserHandle'])){echo $json_a['epicUserHandle'];}else{echo'Introuvable...';};?></h1>
+  <h1> <?php if(isset($json_a['epicUserHandle']) ){echo $json_a['epicUserHandle'];}else{echo'Introuvable...';};?></h1>
 
   <h2>SOLO</h2>
   <p>TOP1 : <?php if(isset($json_a['stats']['p2']['top1']['value'])){ echo $json_a['stats']['p2']['top1']['value'];}else{echo '0';}?> </p>
