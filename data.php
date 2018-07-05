48a02fc6ec0ac1340cd5ec97e93c796136e5b215
Charly <charly.strn@gmail.com>
2018-07-05 09:47:20 +0200

    Auto stash before merge of "master" and "origin/master"

--- data.php
+++ data.php
@@ -21,4 +21,40 @@ if(isset($_POST['pseudo']) && isset($_POST['platform'])){
 
   print_r($json_a);
 }
+
 ?>
+<!DOCTYPE html>
+<html>
+<html lang="fr">
+<head>
+<meta charset="UTF-8">
+
+  <title>Fornite Webservice</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->
+  <link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+  <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+  <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+  <link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<!--===============================================================================================-->
+  <link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+  <link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<!--===============================================================================================-->
+  <link rel="stylesheet" type="text/css" href="css/util.css">
+  <link rel="stylesheet" type="text/css" href="css/main.css">
+<!--===============================================================================================-->
+</head>
+<body>
+  <h1> <?php echo $json_a['epicUserHandle'];?></h1>
+
+  <h2>SOLO</h2>
+  <p>TOP1 : <?php echo $json_a['epicUserHandle']['stats']['p2']['top1'];?> </p>
+
+</body>
+
+</html>
